124408a0ba451448c0a080a8621a4ec6a7637fd4
Richard Mable <richardbmable@gmail.com>
2015-12-08 16:05:21 -0500

    added an alert box for empty birthday field

--- myscript.js
+++ myscript.js
@@ -1,1 +1,25 @@
-(docment)
+$(document).ready(
+	function(){
+		console.log("ready")
+
+	$("form").submit(function(e){
+		e.preventDefault();
+	});
+
+	$("form").submit(function(){
+		if($("#birthday").val() == ""){
+			alert("Please enter a birthday!")
+		}
+
+
+	});
+
+
+
+});
+
+
+
+
+// 06/16/1988 birthday format
+// 0123456789 birthday array order
